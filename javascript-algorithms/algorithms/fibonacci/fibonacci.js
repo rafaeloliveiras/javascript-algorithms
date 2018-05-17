27e118ad0e03c95ad5d8f519195a08c1be313fea
Rafael Oliveira <rafael.oliveiras@live.com>
2018-05-17 04:13:13 -0300

    fix in index

--- javascript-algorithms/algorithms/fibonacci/fibonacci.js
+++ javascript-algorithms/algorithms/fibonacci/fibonacci.js
@@ -3,7 +3,7 @@ var fibonacci = [];
 fibonacci[1] = 1;
 fibonacci[2] = 1;
 
-for (var i = 3; i <= 250; i++) {
+for (var i = 3; i <= 25; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 }
 
